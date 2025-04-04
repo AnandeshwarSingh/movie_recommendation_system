f7435f7c10f3b7d030b4f8a55a6801d728023d0c
VanithaSwamy <vanithaswamy196@gmail.com>
2025-04-04 10:02:01 +0530

    "Added CRUD for User"

--- MovieRecommendationApp/src/main/java/com/example/demo/Repository/UserRepository.java
+++ MovieRecommendationApp/src/main/java/com/example/demo/Repository/UserRepository.java
@@ -1,5 +1,14 @@
 package com.example.demo.Repository;
 
+import java.util.List;
+
+import com.example.demo.Model.UserModel;
+
 public interface UserRepository {
 	
+	public boolean addUser(UserModel user);
+    public List<UserModel> getAllUsers();
+    public UserModel getUserById(int id);
+    public boolean updateUser(UserModel user);
+    public boolean deleteUser(int id);
 }
